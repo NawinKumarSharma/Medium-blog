3f3649550826016b36068f1e69fcdf7082e4ea94
Nawin Kumar Sharma <nawinsharma60@gmail.com>
2024-04-10 00:32:06 +0530

    :sparkles: common folder for zod validation for both FE and BE

---
+++ common/src/index.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+export const signUpInput = z.object({
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be atleast of 6 characters").max(30),
+    name: z.string().optional(),
+});
+
+export const signInInput = z.object({
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be atleast of 6 characters"),
+});
+
+
+export const createBlogInput = z.object({
+    title: z.string().min(1, "Title cannot be empty").max(10),
+    content: z.string().min(1, "Content cannot be empty").max(2000),
+});
+
+
+export const updateBlogInput = z.object({
+    id: z.number().min(1, "Invalid ID, Please enter a valid ID!"),
+    title: z.string().min(1, "Title cannot be empty").max(10),
+    content: z.string().min(1, "Content cannot be empty").max(2000),
+});
+
+
+
+// type inference better for exporting it as types.
+export type signUpInput = z.infer<typeof signUpInput>;
+export type signInInput = z.infer<typeof signInInput>;
+export type createBlogInput = z.infer<typeof createBlogInput>;
+export type updateBlogInput = z.infer<typeof updateBlogInput>;
